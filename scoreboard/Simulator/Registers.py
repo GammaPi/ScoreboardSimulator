b7d4e9262d6b2e0bb119158adc4dcbd1fe514f9c
Steven Tang <mrsteventang@outlook.com>
2020-11-12 18:06:17 -0500

    Finish Register.py

---
+++ scoreboard/Simulator/Registers.py
@@ -0,0 +1,81 @@
+import enum
+from abc import ABCMeta, abstractmethod
+from Simulator.Bus import AbstractBus
+from enum import Enum
+
+
+class RegType(Enum):
+    GP_FLOAT = 1
+    GP_INT = 2
+    SP_PC = 3
+    SP_IR = 5
+    SP_IAR = 7
+    SP_DAR = 8
+
+
+class AbstractRegister(metaclass=ABCMeta):
+    def __init__(self, name, numOfBits):
+        self.numOfBits = numOfBits
+        self.name = name
+
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def write(self, value):
+        pass
+
+
+class GPReigster(AbstractRegister):
+    def __init__(self, name, numOfBits):
+        super().__init__(name, numOfBits)
+        self.value = 0
+
+    def read(self):
+        return self.value
+
+    def write(self, value):
+        self.value = value
+
+
+class IntRegister(GPReigster):
+    def __init__(self, name):
+        super().__init__(name, 32)
+        self.type = RegType.GP_INT
+
+
+class FloatRegister(GPReigster):
+    def __init__(self, name):
+        super().__init__(name, 32)
+        self.type = RegType.GP_FLOAT
+
+
+class SPReigster(AbstractRegister):
+    def __init__(self, name, numOfBits):
+        super().__init__(name, numOfBits)
+        self.value = 0
+
+
+class PC(SPReigster):
+    def __init__(self, name):
+        super().__init__(name, 32)
+        self.type = RegType.SP_PC
+
+
+class IAR(SPReigster):
+    def __init__(self, name):
+        super().__init__(name, 32)
+        self.type = RegType.SP_IAR
+
+
+class DAR(SPReigster):
+    def __init__(self, name):
+        super().__init__(name, 32)
+        self.type = RegType.SP_DAR
+
+
+class IR(SPReigster):
+    def __init__(self, name):
+        super().__init__(name, 32)
+        self.type = RegType.SP_IR
